1c1f980621bd814dda885b829665fc6811fe8795
Yash Kosambia <90350327+yashkosambia14@users.noreply.github.com>
2021-12-28 08:39:26 -0500

    Add files via upload

---
+++ Project_01/project_01.py
@@ -0,0 +1,28 @@
+# Name - YASH JAYESH KOSAMBIA
+# CWID - 10475286
+
+# taking inputs from user using variables x,y,z,k,l
+print("-------------------------------------------------------------------------------------------------------")
+
+
+x = input("Choose a noun: ")
+
+
+y = input("Choose a plural noun: ")
+
+
+z = input("Name a place: ")
+
+
+k = input("Choose an adjective (Describing word): ")
+
+
+l = input("Choose a noun: ")
+
+# printing the received data along with string concatenation as per the instructions
+# used commas as well along with concatenation to try new ways
+
+
+print("Be kind to your "+x, "small "+ y+ " This place", z, "indeed it is", k, l)
+
+print('_______________________________________________________________________________________________________')
